e90fff52752c426c1265c8f32068a9acc6f13477
riccovertaco <42204391+riccovertaco@users.noreply.github.com>
2018-08-08 12:06:36 +0200

    Add files via upload

---
+++ Enable distribution list.ps1
@@ -0,0 +1,8 @@
+﻿Import-module activedirectory
+. 'C:\Program Files\Microsoft\Exchange Server\V15\bin\RemoteExchange.ps1'
+Connect-ExchangeServer -auto
+$groups=Get-adgroup -filter 'groupcategory -eq "Distribution"' -SearchBase "OU=groupes,DC=toto,DC=fr"
+foreach($group in $groups)
+{
+   enable-DistributionGroup -Identity $group.SamAccountName
+}
